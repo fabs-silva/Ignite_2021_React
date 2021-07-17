d8db45a243abd758afde5adcf7f028b365b7ed4a
Fabiana Silva <fabianabz@yahoo.com>
2021-07-17 17:53:29 -0300

    finishing chapter II - challenge 2

--- Chapter-II/02-ignite-reactjs-refatorando-classes/src/components/Input/index.tsx
+++ Chapter-II/02-ignite-reactjs-refatorando-classes/src/components/Input/index.tsx
@@ -6,11 +6,19 @@ import {
 } from 'react';
 
 import { useField } from '@unform/core';
+import { IconType } from 'react-icons';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+
+interface InputProps {
+  name: string;
+  icon?: IconType;
+  placeholder: string;
+}
+
+export function Input({ name, icon: Icon, ...rest }: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -48,6 +56,4 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
-
-export default Input;
+}
